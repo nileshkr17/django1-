33c4ea7702e2c56606b04d9d538f350634c40135
Nilesh <ece.20becd76@silicon.ac.in>
2021-09-24 17:37:42 +0530

    [completed]

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.CharField(max_length=120)
+    phone = models.CharField(max_length=12)
+    desc = models.TextField()
+    date = models.DateField()
+
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
+from datetime import datetime
+from home.models import Contact
 # Create your views here.
 def index(request):
     context = {
@@ -11,10 +12,17 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("this is contact page")
+    return HttpResponse("this is about page")
 
 def contact(request):
-    return render(request,'/form/index.html')
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
+    return render(request,'contact.html')
 
 def services(request):
     return HttpResponse("this is services page")
