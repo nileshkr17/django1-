a6eee73a54edebc303b6e74042a1b717ed8b5e29
Nilesh <ece.20becd76@silicon.ac.in>
2022-03-23 23:08:47 +0530

    [bg-fixed]

--- home/views.py
+++ home/views.py
@@ -15,6 +15,13 @@ def index(request):
 def about(request):
     return HttpResponse("this is about page")
 def login(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
     return render(request,'login.html')
 
 def contact(request):
